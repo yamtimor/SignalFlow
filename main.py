5973ab6fcd156a69232580428e4fd0644f5ef421
yamtimor <yamtimor@gmail.com>
2025-07-21 00:17:11 +0300

    create reddit_client.py due to the hard rate limiter of tradestie

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+from sources.reddit_client import fetch_reddit_posts
 from sources.tradestie_client import fetch_wsb_sentiment
 from sources.twelvedata_client import fetch_stock_data
 from pprint import pprint
@@ -11,10 +12,14 @@ async def main():
     # print(data[0].model_dump())  # Print first entry as dict
     # pprint(data)
 
-    wsb_sentiment = await fetch_wsb_sentiment()
-    print(f"\nWSB Sentiment — {len(wsb_sentiment)} tickers:")
-    pprint(wsb_sentiment)
+    # wsb_sentiment = await fetch_wsb_sentiment()
+    # print(f"\nWSB Sentiment — {len(wsb_sentiment)} tickers:")
+    # pprint(wsb_sentiment)
 
+    print("\n--- Top Reddit Posts ---")
+    reddit_posts = await fetch_reddit_posts()
+    for post in reddit_posts[:30]:  # show only top 5
+        print(post.title)
 
 if __name__ == "__main__":
     asyncio.run(main())
