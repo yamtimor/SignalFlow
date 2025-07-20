fec797056d44debaf18a6b7c8467103992775cd0
yamtimor <yamtimor@gmail.com>
2025-07-20 23:51:59 +0300

    improve the safetyness of the basemodel

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import asyncio
+from sources.tradestie_client import fetch_wsb_sentiment
 from sources.twelvedata_client import fetch_stock_data
 from pprint import pprint
 
 SYMBOL = "VST" 
 
 async def main():
-    data = await fetch_stock_data(SYMBOL)
-    print(f"Fetched {len(data)} data points for {SYMBOL}")
+    # data = await fetch_stock_data(SYMBOL)
+    # print(f"Fetched {len(data)} data points for {SYMBOL}")
     # print(data[0].model_dump())  # Print first entry as dict
-    pprint(data)
+    # pprint(data)
+
+    wsb_sentiment = await fetch_wsb_sentiment()
+    print(f"\nWSB Sentiment — {len(wsb_sentiment)} tickers:")
+    pprint(wsb_sentiment)
+
+
 if __name__ == "__main__":
     asyncio.run(main())
