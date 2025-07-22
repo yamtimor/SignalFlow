8de7c849d73a48e27ff76e02bcc619d6f125ddd1
yamtimor <yamtimor@gmail.com>
2025-07-22 04:17:03 +0300

    fetching all the data sources together in main

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ from pprint import pprint
 SYMBOL = "VST" 
 
 async def main():
-    # data = await fetch_stock_data(SYMBOL)
-    # print(f"Fetched {len(data)} data points for {SYMBOL}")
-    # print(data[0].model_dump())  # Print first entry as dict
-    # pprint(data)
+    data = await fetch_stock_data(SYMBOL)
+    print(f"Fetched {len(data)} data points for {SYMBOL}")
+    print(data[0].model_dump())  # Print first entry as dict
+    pprint(data)
 
-    # wsb_sentiment = await fetch_wsb_sentiment()
-    # print(f"\nWSB Sentiment — {len(wsb_sentiment)} tickers:")
-    # pprint(wsb_sentiment)
+    wsb_sentiment = await fetch_wsb_sentiment()
+    print(f"\nWSB Sentiment — {len(wsb_sentiment)} tickers:")
+    pprint(wsb_sentiment)
 
     print("\n--- Top Reddit Posts ---")
     reddit_posts = await fetch_reddit_posts()
